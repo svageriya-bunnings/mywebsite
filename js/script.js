cd337b1ab26b0c9ce0bb334fe894211cbc8d093b
svageriya-bunnings <svageriya@bunnings.com.au>
2024-04-08 17:31:52 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// js/script.js
+
+document.getElementById('changeColor').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
