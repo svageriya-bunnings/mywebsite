1fb29cd12967d541dc77bd9bc1da1c95aeec1859
svageriya-bunnings <svageriya@bunnings.com.au>
2024-04-09 11:47:00 +1000

    more feature added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // js/script.js
 
-document.getElementById('changeColor').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+let counter = 0;
+
+document.getElementById('like').addEventListener('click', function() {
+    counter++;
+    document.getElementById('visitorCount').innerText = "Visitor Count: " + counter;
 });
