1ede21797e2e2ac85f00ad09c1d30ab6649930aa
Sahil Vageriya <SVageriya@bunnings.com.au>
2025-06-02 17:07:48 +1000

    v1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 // js/script.js
 
-let counter = 0;
+let counter = localStorage.getItem('visitorCount') ? parseInt(localStorage.getItem('visitorCount')) : 0;
+document.getElementById('visitorCount').innerText = "Visitor Count: " + counter;
 
 document.getElementById('like').addEventListener('click', function() {
     counter++;
     document.getElementById('visitorCount').innerText = "Visitor Count: " + counter;
+    localStorage.setItem('visitorCount', counter);
 });
